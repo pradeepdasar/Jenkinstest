4352363dfd144be0d1f0d4dc719528a97ab7dd91
pradeepdasar <saipradeep6223@gmail.com>
2024-10-31 07:31:54 +0530

    thirdcommit

---
+++ BudzDemo1.java
@@ -0,0 +1,21 @@
+
+public class BudzDemo1
+{
+
+	public static void main(String[] args)
+	{
+int grossalary=100000;
+int netsalary=grossalary-totalcharges;
+
+void housecharges()
+{
+	int rent=20000;
+	int childfees=10000;
+	int totalcharges=rent+childfees;
+	System.out.println(totalcharges);
+	
+
+
+	}
+
+}
